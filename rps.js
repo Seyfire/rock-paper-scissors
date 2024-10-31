c900879ec9de19641fb216ccb34270f2ac05edc6
seyfire <97469939+Seyfire@users.noreply.github.com>
2024-10-30 22:10:06 -0400

    refactor playRound into two seperate functions

--- rps.js
+++ rps.js
@@ -37,25 +37,70 @@ function getHumanChoice() {
     num = parseInt(prompt("Enter 1 for Rock, 2 for Paper, and 3 for Scissors"));
 
     // init empty string variable
-    let human_choice = "";
+    let humanChoice = "";
 
     // use input num to assign corresponding string to var
     switch (num) {
         case 1:
-            human_choice = "rock";
+            humanChoice = "rock";
             break;
         case 2:
-            human_choice = "paper";
+            humanChoice = "paper";
             break;
         case 3:
-            human_choice = "scissors";
+            humanChoice = "scissors";
             break;
         default:
             console.log("Invalid Number");        
     }
 
-    return human_choice;
+    return humanChoice;
+}
+function playRound (humanChoice, computerChoice) {
+    // plays a single round of RPS
+
+    //print message showing what each player picked
+    console.log(
+        "You picked: " + humanChoice + ".\nThe computer picked: "
+        + computerChoice + ".\n"
+    )
+
+    // compare human vs computer choice, and store outcome in var
+    const outcome = checkWinner(humanChoice, computerChoice);
+    // print message and increment score based on outcome of round
+    switch (outcome) {
+        case 2:
+            console.log("Tie game!\n");
+            break;
+        case 1:
+            console.log("You Win!\n");
+            humanScore = humanScore + 1;
+            break;
+        case 0:
+            console.log("You lost :(\n");
+            computerScore = computerScore + 1;
+    }
 }
 
-console.log("The computer chose: " + getComputerChoice() + ".\n");
-console.log("You chose: " + getHumanChoice() + ".\n");
+function checkWinner (humanChoice, computerChoice) {
+    // returns 1 if human wins, 2 if tie, otherwise 0
+    if (humanChoice == computerChoice) {
+        return 2;
+    } else if (
+               (humanChoice == "rock" && computerChoice == "scissors")
+            || (humanChoice == "paper" && computerChoice == "rock")
+            || (humanChoice == "scissors" && computerChoice == "paper")
+        ) {
+                return 1;
+    } else {
+        return 0;
+    }
+}
+
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
+
+let humanScore = 0;
+let computerScore = 0;
+
+playRound(humanSelection, computerSelection);
